e641d80a563a06536882cafe818f4899729b11a9
ignacio pugliese <ignacio.pugliese@10pines.com>
2021-03-16 00:24:27 -0300

    pum! now sharkie remembers where he was

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const { app, BrowserWindow } = require('electron')
+const ipc = require('electron').ipcMain
+const Menu = require('electron').Menu
+const MenuItem = require('electron').MenuItem
+const windowStateKeeper = require('electron-window-state');
+
 let win = null;
 
 function createWindow () {
+
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 240,
+    defaultHeight: 240
+  });
+
   win = new BrowserWindow({
-    width: 240,
-    height: 240,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    x:mainWindowState.x,
+    y:mainWindowState.y,
     frame:false,
     resizable:false,
     transparent:true,
@@ -14,14 +27,13 @@ function createWindow () {
 
     }
   })
-  //win.webContents.openDevTools()
   win.loadFile('index.html')
   win.setMenu(null);
   win.setSkipTaskbar(true);
   win.isMenuBarVisible(false);
+  mainWindowState.manage(win);
 
 }
-
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -32,10 +44,6 @@ app.on('window-all-closed', () => {
 app.on('ready', function () {
   createWindow()
 });
-
-
-const Menu = require('electron').Menu
-const MenuItem = require('electron').MenuItem
 
 
 const menu = new Menu()
@@ -60,19 +68,23 @@ const smallMenuItem = new MenuItem({
   }
 })
 
+const close = new MenuItem({
+  label: 'kill sharkie :(',
+  click: () => {
+    app.quit()
+  }
+})
 
 menu.append(bigMenuItem)
 menu.append(mediumMenuItem)
 menu.append(smallMenuItem)
-
+menu.append(close)
 
 function resizeWindow(size) {
   win.setResizable(true);
   win.setSize(size, size);
   win.setResizable(false);
 }
-
-const ipc = require('electron').ipcMain
 
 ipc.on('right-click', () => {
   menu.popup(win)
